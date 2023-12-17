57fe14524e4f4763ea100e1b52d3e18b43a9a3b2
sr1983-it-gl <sr1983.it.gl@gmail.com>
2023-12-17 10:42:49 +0530

    Lab02 - Creation of Question objects

--- 04-Lab-02/Quiz-App/quiz.js
+++ 04-Lab-02/Quiz-App/quiz.js
@@ -6,3 +6,14 @@ function Question(questionText) {
 }
 
 const question1 = new Question("JavaScript supports");
+const question2 = new Question("Which language is used for styling web pages ?")
+const question3 = new Question("Which is not a JavaScript framework ?")
+const question4 = new Question("Which is used to connect to Database ?")
+const question5 = new Question("JavaScript is a ?");
+
+/*
+const question2 
+const question3
+const question4
+const question5
+*/
