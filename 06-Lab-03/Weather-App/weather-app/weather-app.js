ad045bdd11671ff951cc6092c963f12557b39eac
sr1983-it-gl <sr1983.it.gl@gmail.com>
2024-01-14 12:10:40 +0530

    Integrating front-end with backend code

---
+++ 06-Lab-03/Weather-App/weather-app/weather-app.js
@@ -0,0 +1,32 @@
+
+
+class WeatherApp {
+
+  addListeners(){
+
+    // Get a reference to text-field
+    // keypress
+    // Italy [Enter]
+    // event.key == "Enter"
+  }
+
+  handleEvent(){
+
+    // Call the Weather-API.
+      // buildURL
+      // invokeURL
+    // responseJSON
+  }
+
+
+  updateUI(){
+
+    // responseJSON
+    // extract all the values
+      // Get a reference to the HTML fields
+      // Update the HTML fields
+  }
+
+}
+
+export {WeatherApp}
