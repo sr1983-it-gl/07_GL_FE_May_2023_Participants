5aab0f487708a8aeeb659802989d99ed9865ea78
sr1983-it-gl <sr1983.it.gl@gmail.com>
2024-02-11 10:55:33 +0530

    Fetching expense items and printing in the console

--- 08-Lab-04/Expense-Tracker/client/src/components/ExpenseTrackerApp.js
+++ 08-Lab-04/Expense-Tracker/client/src/components/ExpenseTrackerApp.js
@@ -2,6 +2,11 @@
 
 const ExpenseTrackerApp = () => {
 
+  // Write down the useEffect hook
+  // Write a new method within the useEffect
+  // Within the new method impl, call the service method
+  // Print the results
+
   return (
     <div>
       Expense Tracker App
