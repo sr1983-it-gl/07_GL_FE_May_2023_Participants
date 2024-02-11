30aeb210bf7c73b063a8a76c9877ef5c4872c45d
sr1983-it-gl <sr1983.it.gl@gmail.com>
2024-02-11 12:37:54 +0530

    Saving Data -- Part I - Creation of POST method

--- 08-Lab-04/Expense-Tracker/client/src/services/expense.js
+++ 08-Lab-04/Expense-Tracker/client/src/services/expense.js
@@ -10,4 +10,16 @@ const getAllExpenseItems = async () => {
 
 }
 
-export {getAllExpenseItems}
+const createNewExpenseItem = (newExpenseItem) => {
+
+  const POST_EXPENSES_URL = "http://localhost:4000/expenses";
+
+  axios.post(POST_EXPENSES_URL, newExpenseItem, {
+    headers : {
+      'Content-Type' : 'application/json'
+    }
+  })
+
+}
+
+export {getAllExpenseItems, createNewExpenseItem}
