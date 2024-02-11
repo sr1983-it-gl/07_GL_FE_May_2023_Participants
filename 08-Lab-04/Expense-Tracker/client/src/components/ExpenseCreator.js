65943a4d8124a629b28bde6abcd612ea49acd870
sr1983-it-gl <sr1983.it.gl@gmail.com>
2024-02-11 12:18:33 +0530

    Expense Creation - UI - Part II

--- 08-Lab-04/Expense-Tracker/client/src/components/ExpenseCreator.js
+++ 08-Lab-04/Expense-Tracker/client/src/components/ExpenseCreator.js
@@ -3,12 +3,55 @@ import Button from 'react-bootstrap/Button';
 import { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 
+import Form from 'react-bootstrap/Form';
+
 const ExpenseCreator = () => {
 
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  function expenseForm() {
+    return (
+      <Form>
+
+        <Form.Group className="mb-3" controlId="expenseDescription">
+          <Form.Label>Expense Description</Form.Label>
+          <Form.Control type="text" placeholder="Enter expense description" />
+        </Form.Group>
+  
+        <Form.Group className="mb-3" controlId="payeeName">
+          <Form.Label>Payee Name</Form.Label>
+          
+          <Form.Select aria-label="Default select example">
+            <option>Open this select menu</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </Form.Select>
+
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="price">
+          <Form.Label>Price</Form.Label>
+          <Form.Control type="number" placeholder="Enter expense price" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="expenseDate">
+          <Form.Label>Expense Date</Form.Label>
+          <Form.Control type="date"/>
+        </Form.Group>
+
+        <Button variant="secondary" onClick={handleClose}>
+            Close
+        </Button>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    );
+  }
 
   return (<div>
 
@@ -18,17 +61,11 @@ const ExpenseCreator = () => {
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Create New Expense</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
+        <Modal.Body>
+          {expenseForm()}
+        </Modal.Body>
       </Modal>
 
   </div>)
