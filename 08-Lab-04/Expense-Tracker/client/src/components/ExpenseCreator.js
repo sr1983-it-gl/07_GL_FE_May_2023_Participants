6a7ca25c985b72ff3044a4883d19b5418ddc8bca
sr1983-it-gl <sr1983.it.gl@gmail.com>
2024-02-11 12:59:14 +0530

    Saving Data -- Part II

--- 08-Lab-04/Expense-Tracker/client/src/components/ExpenseCreator.js
+++ 08-Lab-04/Expense-Tracker/client/src/components/ExpenseCreator.js
@@ -5,7 +5,11 @@ import Modal from 'react-bootstrap/Modal';
 
 import Form from 'react-bootstrap/Form';
 
+import {useRef} from "react"
+
 import {getAllPayeeNames} from "../services/expense-utils"
+
+import {createNewExpenseItem} from "../services/expense";
 
 const ExpenseCreator = ({expenseItems}) => {
 
@@ -16,19 +20,46 @@ const ExpenseCreator = ({expenseItems}) => {
 
   const payeeNames = getAllPayeeNames(expenseItems)
 
+  const expenseDescriptionRef = useRef(null);
+  const payeeNameRef = useRef(null);
+  const priceRef = useRef(null);
+  const dateRef = useRef(null);
+
+  const handleNewExpenseCreation = async (event) => {
+
+    event.preventDefault();
+
+
+    const expenseDescription = expenseDescriptionRef.current.value;
+    const payeeName = payeeNameRef.current.value;
+    const price = parseFloat(priceRef.current.value);
+    const date = new Date(dateRef.current.value)
+
+    const newExpenseItem = {
+      expenseDescription: expenseDescription,
+      payeeName: payeeName,
+      price: price,
+      date: date
+    }
+    const response = await createNewExpenseItem(newExpenseItem)
+    console.log('Response is ' + JSON.stringify(response));
+
+    handleClose();
+  }
+
   function expenseForm() {
     return (
-      <Form>
+      <Form onSubmit={handleNewExpenseCreation}>
 
         <Form.Group className="mb-3" controlId="expenseDescription">
           <Form.Label>Expense Description</Form.Label>
-          <Form.Control type="text" placeholder="Enter expense description" />
+          <Form.Control type="text" placeholder="Enter expense description" ref={expenseDescriptionRef}/>
         </Form.Group>
   
         <Form.Group className="mb-3" controlId="payeeName">
           <Form.Label>Payee Name</Form.Label>
           
-          <Form.Select aria-label="Default select example">
+          <Form.Select aria-label="Default select example" ref={payeeNameRef}>
             <option>SELECT A PAYEE</option>
 
             {
@@ -45,12 +76,12 @@ const ExpenseCreator = ({expenseItems}) => {
 
         <Form.Group className="mb-3" controlId="price">
           <Form.Label>Price</Form.Label>
-          <Form.Control type="number" placeholder="Enter expense price" />
+          <Form.Control type="number" placeholder="Enter expense price" ref={priceRef}/>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="expenseDate">
           <Form.Label>Expense Date</Form.Label>
-          <Form.Control type="date"/>
+          <Form.Control type="date" ref={dateRef} />
         </Form.Group>
 
         <Button variant="secondary" onClick={handleClose}>
--- 08-Lab-04/Expense-Tracker/client/src/services/expense.js
+++ 08-Lab-04/Expense-Tracker/client/src/services/expense.js
@@ -10,16 +10,17 @@ const getAllExpenseItems = async () => {
 
 }
 
-const createNewExpenseItem = (newExpenseItem) => {
+const createNewExpenseItem = async (newExpenseItem) => {
 
   const POST_EXPENSES_URL = "http://localhost:4000/expenses";
 
-  axios.post(POST_EXPENSES_URL, newExpenseItem, {
+  const response = await axios.post(POST_EXPENSES_URL, newExpenseItem, {
     headers : {
       'Content-Type' : 'application/json'
     }
   })
 
+  return response.data;
 }
 
 export {getAllExpenseItems, createNewExpenseItem}
